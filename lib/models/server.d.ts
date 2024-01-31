b56aec9a52cba09db71213b52088e688e34cd4c6
octanuary <94416681+octanuary@users.noreply.github.com>
2024-01-31 01:07:38 -0600

    another ts fix

--- lib/models/server.d.ts
+++ lib/models/server.d.ts
@@ -5,15 +5,13 @@ declare type httpzOptions = {
 	strictUrl?: boolean
 };
 
-declare class Server extends Group {
+export class Server extends Group {
 	constructor(options: httpzOptions);
 
 	/**
 	 * Alias of http.Server.listen().
 	 */
-	listen(...p): void;
+	listen(): typeof httpServer.prototype.listen;
 
 	server: httpServer
 }
-
-export = Server;
